507d20db02bd61dfc258f61c7c2eee05950efd0d
hasolanor <37587078+hasolanor@users.noreply.github.com>
2020-12-01 17:21:23 -0500

    Add files via upload

--- fitting_retention_parameters.m
+++ fitting_retention_parameters.m
@@ -1,9 +1,36 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Fitting Retention Parameters 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Authors: Hillmert Solano, Matteo Icardi, Juan M. Mejía
+%Institutions: University of Nottingham & Universidad Nacional de Colombia
+%Corresponding mail: hasolanor@unal.edu.co
 
+%DESCRIPTION
+%This algorithm seeks a set of retention model parameters that allows to
+%reach the minimum error between retention models and experimental data.
+%PDE-based models are solved using ChebFun package.
 
+%FUNDING
+%Industry  Academia  Partnership  Programme  -  17/18(IAPP100080)
+%Royal Academy of Engineering of the United Kingdom
+%Project: Development of a robust modelling and up-scaling techniques for 
+%studying nanofluids transportand retention in porous rocks for subsurface
+%engineering applications
 
+%REFERENCES
+%Solano, H.A., Icardi, M., Mejía J.M.: Nanoparticle transport and retention
+%in porousmedia: Darcy-scale modelling and dimensionalanalysis. Submitted
+%to publication (2020).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Zhang,  T.:  Modeling  of  Nanoparticle  Transport  in  Porous  Media.   
+%PhD  Dissertation,The University of Texas at Austin (2012).
+%URL http://hdl.handle.net/2152/ETD-UT-2012-08-6044
+
+%Platte, R.B., Trefethen, L.N.: Chebfun: A New Kind of Numerical Computing.
+%In: A.D.Fitt, J. Norbury, H. Ockendon, E. Wilson (eds.) Progress in 
+%Industrial Mathematics atECMI 2008, Mathematics in Industry, pp. 69–87. 
+%Springer, Berlin, Heidelberg (2010).DOI 10.1007/978-3-642-12110-45.  
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 close all
 clear all
@@ -43,4 +70,3 @@ iPe=[0.0133,0.1356];
 %Optimisation algorithm. Return the best array of model parameters matching
 %with the experimental data, according to the seed 
 [optimisation]=NRM_optimisation(exp(1),iPe,model,cond(1),x,init(1));
-
